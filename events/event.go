362621de86794f80f757566b6566a6ad29618ff3
Ashen Nilura <52426498+Nilura@users.noreply.github.com>
2024-02-28 13:12:22 +0530

    Update event.go

--- events/event.go
+++ events/event.go
@@ -74,21 +74,15 @@ func HandleEvent(w http.ResponseWriter, r *http.Request) {
 	      //  fields := blocks["fields"].([]interface{})
 		block := blocks[0].(map[string]interface{})
 		fmt.Println("Blocks text:", block)
-		// fields, ok := blocks["fields"].([]interface{})
-		// if !ok || len(fields) < 1 {
-		// 	fmt.Println("No fields found in the block")
-		// 	return
-		// }
-
-		// Extracting the text from the first field
-		// firstField := fields[0].(map[string]interface{})
-		// text, ok := firstField["text"].(string)
-		// if !ok {
-		// 	fmt.Println("Text not found in the first field")
-		// 	return
-		// }
-
-		// fmt.Println("Extracted text:", text)
+		fields := block["fields"].([]interface{})
+			fmt.Println("Blocks text:", block)
+		 len(fields) > 0 {
+		    firstField := fields[0].(map[string]interface{})
+		    text := firstField["text"].(string)
+		    fmt.Println("Text of the first field:", text)
+		} else {
+		    fmt.Println("No fields found in the block")
+		}
 
 		userData, ok := payload["authorizations"].([]interface{})
 		if !ok || len(userData) == 0 {
